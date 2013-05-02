dc275482c0440bd03303bc5a0e209b22d5ddfcb7
Shown <hles51723@yahoo.com.tw>
2013-05-02 18:36:26 +0800

    #2 web template design
    create master css

---
+++ apps/views/layout/template.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<script type="application/javascript" src="js/jquery-1.9.1.js"></script>
+		<link rel="stylesheet" href="css/master.css" />
+
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<div id="logoBox"></div>
+				<div id="nav"></div>
+
+			</div>
+			<div id="menuBox"></div>
+			<div id="wrap"></div>
+		</div>
+		<p class="footer"></p>
+		</div>
+	</body>
+</html>
