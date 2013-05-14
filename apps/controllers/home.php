f8a3adb075a6764ad6b3ddf428077c1f17be3e5c
shown <hles51723@yahoo.com.tw>
2013-05-14 16:57:42 +0800

    #2 update 更改home 選單 新增空白layout

--- apps/controllers/home.php
+++ apps/controllers/home.php
@@ -10,9 +10,10 @@ class HomeController extends MY_Controller {
 	public function index() {
 
 		$itemList = array("itemList"=>array(
-			"item1" => array("member","./index.php/member", "會員管理"),
-			"item2" => array("news","./index.php/news", "公告管理"),
-			"item3" => array("logout","./", "登出帳號")
+			array("member","./index.php/member", "會員管理"),
+			array("news","./index.php/news", "公佈欄管理"),
+			array("exam","./index.php/exam", "試題庫管理"),
+			array("logout","./", "登出帳號")
 			));
 
 		$this -> layout -> view('view/home/default', $itemList);
