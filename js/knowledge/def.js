ca91b6837416ae741ab5f37da345a56a0e9129dc
Eric <doshajou@gmail.com>
2013-07-09 18:09:01 +0800

    修正箭頭移位的問題
    Signed-off-by: Eric <doshajou@gmail.com>

--- js/knowledge/def.js
+++ js/knowledge/def.js
@@ -1467,7 +1467,7 @@ function setLine(lineId, p1, p2) {
 	})
 	
 	$(line).find(".lineArrow").css({
-		'left' : 45,
+		'left' : 45 + (zoomCount * 3),
 		'transform' : 'rotate(' + 45 + 'deg)'
 	})
 	
@@ -1496,7 +1496,7 @@ function setChildLine(lineId, p1, p2) {
 	})
 	
 	$(line).find(".lineArrow").css({
-		'left' : 25,
+		'left' : (zoomCount * 13),
 		'transform' : 'rotate(' + 45 + 'deg)'
 	})
 	
